7ebd21a114c04aaed5ef3c5ca60eb638afe5e079
MANI <chandramanigupta2003@gmail.com>
2024-09-13 07:07:08 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@
 // import Pr4 from "./components/pr4";
 // import Pr6 from "./components/pr6";
 // import Pr7 from "./components/pr7";
-import Pr8 from "./components/pr8";
+// import Pr8 from "./components/pr8";
+// import Pr9 from "./components/pr9";
 export default function App() {
+  // const items = ["Item 1", "Item 2", "Item 3", "Another item 4"];
   return (
     <div>
       {/* <Pr1 /> */}
@@ -15,7 +17,9 @@ export default function App() {
       {/* <Pr4 /> */}
       {/* <Pr5/> */}
       {/* <Pr7 /> */}
-      <Pr8 />
+
+      {/* <Pr9 items={items} /> */}
+      {/* <Pr8 /> */}
     </div>
   );
 }
